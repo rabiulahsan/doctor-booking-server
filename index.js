a65b7943572f84908a9d2fca7e16153ae697b271
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-12-15 13:50:45 +0600

    connected to database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { run } = require("./utils/dbconnection");
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,8 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+run();
 
 //test
 app.get("/", (req, res) => {
